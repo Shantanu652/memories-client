bdb268ac98373f38eb6c0b21ae4bfe8ac4bd4ee3
Shantanu <sb652002@gmail.com>
2024-03-07 12:52:44 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 
@@ -7,15 +7,16 @@ import memories from './images/memories.png'
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles'
+// import { set } from 'mongoose';
 
 const App = () => {
-
+  const [ currentId, setCurrentId ] = useState(null);
   const classes = useStyles();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch])
+  }, [currentId, dispatch])
 
   return (
     <Container maxwidth="lg">
@@ -27,10 +28,10 @@ const App = () => {
         <Container>
           <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
             <Grid item xs={12} sm={7}>
-              <Posts/>
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form/>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
