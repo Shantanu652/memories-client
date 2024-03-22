eb800af6e8ea409a500539587a86db6dcc9256f9
Shantanu <sb652002@gmail.com>
2024-03-23 02:18:56 +0530

    alex

---
+++ allex.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container } from '@material-ui/core'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+// import { set } from 'mongoose';
+
+const App = () => {
+
+    return (
+
+    <BrowserRouter>
+    <Container maxwidth="lg">
+        <Navbar/>
+        <Routes>
+        <Route path='/' exact Component={Home} />
+        <Route path='/auth' exact Component={Auth} />
+        </Routes>
+    </Container>
+    </BrowserRouter>
+    ) 
+}
+
+export default App
